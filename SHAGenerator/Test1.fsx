0e1e1e2e23601e742b350320b772db2ecdbbfc3f
shreyansjain1407 <42056701+shreyansjain1407@users.noreply.github.com>
2021-09-20 12:09:11 -0400

    .

--- SHAGenerator/Test1.fsx
+++ SHAGenerator/Test1.fsx
@@ -6,12 +6,20 @@ for arg in fsi.CommandLineArgs |> Seq.skip 1 do
     File.ReadAllBytes(arg) |> (new SHA256Managed()).ComputeHash |> Seq.iter (printf "%x")
     printfn ""
 
-let str_byt (str:String) = 
-    System.Text.Encoding.ASCII.GetBytes(str)
+let str_byt (str:string) = 
+    let out = System.Text.Encoding.ASCII.GetBytes(str)
+    out
 
 let sb = System.Text.StringBuilder("ShreyansJain")
 
-printfn File.ReadAllBytes("inp.txt")
+// printfn File.ReadAllBytes("inp.txt")
 // let hash = File.ReadAllBytes("inp.txt") |> (new SHA256Managed()).ComputeHash
-let hash = System.Text.Encoding.ASCII.GetBytes(sb.ToString()) |> (new SHA256Managed()).ComputeHash |> Seq.iter (printf "%x")
-printfn hash
+// let hash = System.Text.Encoding.ASCII.GetBytes(sb.ToString()) |> (new SHA256Managed()).ComputeHash |> Seq.iter (printf "%x")
+let hash' = sb.ToString() |> str_byt |> (new SHA256Managed()).ComputeHash |> Seq.iter (printf "%x")
+
+let intToChar c = 
+    char c
+
+printfn "%c" intToChar 48
+for i = 1 to 10 do
+    printfn "%i" i
