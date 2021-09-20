62f13df9d3994a1c3988ba46dcfed6eec66afaf8
shreyansjain1407 <42056701+shreyansjain1407@users.noreply.github.com>
2021-09-19 21:27:37 -0400

    StringBuilder added

--- SHAGenerator/Test1.fsx
+++ SHAGenerator/Test1.fsx
@@ -6,5 +6,12 @@ for arg in fsi.CommandLineArgs |> Seq.skip 1 do
     File.ReadAllBytes(arg) |> (new SHA256Managed()).ComputeHash |> Seq.iter (printf "%x")
     printfn ""
 
-let hash = File.ReadAllBytes("inp.txt") |> (new SHA256Managed()).ComputeHash |> Seq.iter (printf "%x")
+let str_byt (str:String) = 
+    System.Text.Encoding.ASCII.GetBytes(str)
+
+let sb = System.Text.StringBuilder("ShreyansJain")
+
+printfn File.ReadAllBytes("inp.txt")
+// let hash = File.ReadAllBytes("inp.txt") |> (new SHA256Managed()).ComputeHash
+let hash = System.Text.Encoding.ASCII.GetBytes(sb.ToString()) |> (new SHA256Managed()).ComputeHash |> Seq.iter (printf "%x")
 printfn hash
