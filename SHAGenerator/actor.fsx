26955b6ace3df518de5956827e0f707370bdbca3
marcuselosegui <notmarcus@ufl.edu>
2021-09-22 17:41:22 -0400

    added actor message to rec loop

--- SHAGenerator/actor.fsx
+++ SHAGenerator/actor.fsx
@@ -1,4 +1,4 @@
-﻿//#time "on"
+﻿#time "on"
 #r "nuget: Akka.FSharp" //load with nuget
 #load "sha256.fsx"
 #load "RandomString.fsx"
@@ -11,46 +11,53 @@ open RandomString
 
 let system = System.create "system" <| Configuration.load()
 
-type ProcessorMessage = ProcessJob of int * int
+type MinerMessage = MineJob of int * int
+type BossMessage = BossJob of bool * string * string
 
-let processor (mailbox : Actor <_>) =
+let scriptArg = fsi.CommandLineArgs
+let numLead = scriptArg.[1] |> int
+//printf "Arg: %s\n" scriptArg.[1]
+
+let Miner (mailbox : Actor <_>) =
     let rec loop() = actor {
-        let! ProcessJob(numZero, strLen) = mailbox.Receive()
-        // Handle message here
-        //printfn "Processor: Received ProcessJob %i %i %i" x y z
+        let! MineJob(numZero, strLen) = mailbox.Receive()
+        let boss = mailbox.Sender()
         let e = inputStr strLen
-        printf "%s\n" e
+        //printf "%s  " e
         let d = stringToHash e
-        printf "%s\n" d
-        
+        //printf "%s\n" d
+        let found = leadCheck (d, numZero)
+        if(found) then 
+            //printf "Found Miner: %s : %s\n" e d
+            boss <! BossJob(found, e, d)
+        boss <! BossJob(found, e, d)
         return! loop()
     }
     loop()
 
-let processorRef = spawn system "processor" processor
+//let processorRef = spawn system "miner" Miner
 
-processorRef <! ProcessJob(1,5)
+//processorRef <! MineJob(1,5)
 
+let Boss (mailbox : Actor <_>) = 
+    let numProcess = System.Environment.ProcessorCount |> int
+    let numMiners = numProcess*250
+    let minerArray = Array.create numMiners (spawn system "miner" Miner)
 
-(*
-let strategy =
-    Strategy.OneForOne (fun e ->
-    match e with
-    | :? ArithmeticException -> Directive.Resume
-    | :? ArgumentException -> Directive.Stop
-    | _ -> Directive.Escalate)
+    let rand = new Random()
+    for i in minerArray do
+               i <! MineJob(numLead,rand.Next(100))
+    let rec loop() = actor {
+        let! BossJob(found, input, hash) = mailbox.Receive()
+       
+        if (found) then
+            printf "Found Boss: %s : %s\n" input hash 
+            mailbox.Context.System.Terminate() |> ignore
+        for i in minerArray do
+               i <! MineJob(numLead,rand.Next(100))
+        return! loop()
+    } loop()
 
-let boss =
-    spawnOpt system "master" (fun mailbox ->
-        let worker = spawn mailbox "worker" workerFun
-        
-        let rec loop() =
-            actor {
-                let! msg = mailbox.Receive()
-                match msg with
-                | Responnd -> worker.Tell(msg, mailbox.Sender())
-                | _ -> worker <! msg
-                return! loop()
-            }
-        loop()) [SupervisorStrategy(strategy)]
-        *)
+let bossRef = spawn system "boss" Boss
+
+system.WhenTerminated.Wait()
