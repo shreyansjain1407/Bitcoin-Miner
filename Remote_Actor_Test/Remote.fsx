1eee31c419d402588c83f64dce533f05f17e77ae
Shreyans Jain <42056701+shreyansjain1407@users.noreply.github.com>
2021-09-23 15:12:22 -0400

    Remote Test Error

---
+++ Remote_Actor_Test/Remote.fsx
@@ -0,0 +1,22 @@
+﻿#r "nuget: Akkling" 
+#r "nuget: Akka.Remote"
+#r "nuget: Newtonsoft.Json"
+
+open System
+open Akkling
+
+let config =
+    Configuration.parse
+        @"akka {
+            actor.provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
+            remote.helios.tcp {
+                hostname = localhost
+                port = 9001
+            }
+        }"
+        
+[<EntryPoint>]
+let main args =
+    use system = System.create "remote-system" config
+    System.Console.ReadLine()
+    0
---
+++ Remote_Actor_Test/localInstance.fsx
@@ -0,0 +1,31 @@
+﻿#r "nuget: Akkling" 
+#r "nuget: Akka.Remote"
+#r "nuget: Newtonsoft.Json"
+
+open System
+open Akkling
+open Akka.Actor
+// return Deploy instance able to operate in remote scope
+let deployRemotely address = Deploy(RemoteScope (Address.Parse address))
+
+let spawnRemote systemOrContext remoteSystemAddress actorName expr =
+    spawne systemOrContext actorName expr [SpawnOption.Deploy (deployRemotely remoteSystemAddress)]
+
+let system = System.create "local-system" config
+let aref =
+    spawnRemote system "akka.tcp://remote-system@localhost:9001/" "hello"
+       // actorOf wraps custom handling function with message receiver logic
+       <@ actorOf (fun msg -> printfn "received '%s'" msg) @>
+
+// send example message to remotely deployed actor
+aref <! "Hello world"
+
+// thanks to location transparency, we can select 
+// remote actors as if they where existing on local node
+let sref = select "akka://local-system/user/hello" system
+sref <! "Hello again"
+
+// we can still create actors in local system context
+let lref = spawn system "local" (actorOf (fun msg -> printfn "local '%s'" msg))
+// this message should be printed in local application console
+lref <! "Hello locally"
